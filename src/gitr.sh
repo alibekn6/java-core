7c4e7a525de31d2205e649b88f06e042f3b64f3e
Alibek <alibekandanuarbek@gmail.com>
2025-02-17 19:35:46 +0500

    some new commit from terminal

--- src/gitr.sh
+++ src/gitr.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Ensure at least one argument is provided
+if [ $# -eq 0 ]; then
+  echo "Error: Commit message is required."
+  echo "Usage: ./commit.sh <commit-message>"
+  exit 1
+fi
+
+# Combine all arguments into a single commit message
+commit_message="$*"
+
 git add -A
-git commit -m ""
+git commit -m "$commit_message"
 git push
