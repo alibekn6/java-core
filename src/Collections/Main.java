325f4faa0cd7e39f788064ee903c6528b059fe44
Alibek <alibekandanuarbek@gmail.com>
2025-02-28 13:37:59 +0500

    collections java

--- src/Collections/Main.java
+++ src/Collections/Main.java
@@ -1,22 +1,72 @@
 package Collections;
 
+import Collections.Setik;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        String[] arr = new String[10];
+        arr[0] = "a";
+        arr[1] = "b";
+        arr[2] = "c";
+        arr[3] = "d";
+        arr[4] = "e";
+
+        ArrayList
+
+        String toBeAdded = "b";
+
+        boolean alreadyExists = false;
+        for (String s : arr) {
+            if (s == null) {
+                break;
+            }
+            if (s.equals(toBeAdded)) {
+                alreadyExists = true;
+                break;
+            }
+        }
+
+        if (!alreadyExists) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == null) {
+                    arr[i] = toBeAdded;
+                    break;
+                }
+            }
+        }
 
 
 
 
-
+        print(arr);
 
     }
 
+    public static void print(String[] array) {
+        for (String element : array) {
+            if (element != null) {
+                System.out.println(element);
+            }
+        }
+    }
 
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
 
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+
+        return false;
+    }
 
 
 }
+
 
 interface Animal {
     void eat();
@@ -41,11 +91,6 @@ class shit {
         double elapsedTimeInMillis = elapsedTime / 1_000_000.0;
         System.out.println(elapsedTimeInMillis);
 
-
-
-
-
-
         long startTime2 = System.nanoTime();
 
         // code
@@ -56,18 +101,11 @@ class shit {
             list2.add(c);
         }
 
-
-
-
-
         long endTime2 = System.nanoTime();
 
         long elapsedTime2 = endTime2 - startTime2;
         double elapsedTimeInMillis2 = elapsedTime2 / 1_000_000.0;
         System.out.println(elapsedTimeInMillis2);
-
-
-
 
         Vector<Integer> vector = new Vector<>();
         vector.add(121);
---
+++ src/Collections/Setik.java
@@ -0,0 +1,110 @@
+package Collections;
+
+import java.util.Arrays;
+
+public class Setik {
+    private int DEFAULT_SIZE = 5;
+    private int size = 0;
+    private String[] set;
+
+    public Setik(int size) {
+        this.DEFAULT_SIZE = size;
+        this.set = new String[DEFAULT_SIZE];
+    }
+
+    public Setik() {
+        this.set = new String[DEFAULT_SIZE];
+    }
+
+
+    public boolean add(String element) {
+
+        if (contains(element)) return false;
+        if (size >= set.length) resize();
+        set[size++] = element;
+        return true;
+
+    }
+
+    public boolean remove(String element) {
+        for (int i = 0; i < size; i++) {
+            if (set[i].equals(element)) {
+                for (int j = i; j < size-1; j++) {
+                    set[j] = set[j+1];
+                }
+                set[size - 1] = null;
+                size--;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void print() {
+        for (String element : set) {
+            if (element == null) continue;
+            System.out.println(element);
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+
+    public boolean contains(String element) {
+        for (int i = 0; i < size; i++) {
+            if (set[i].equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void resize() {
+        int newSize = set.length * 2;
+        set = Arrays.copyOf(set, newSize);
+    }
+
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            set[i] = null;
+        }
+        size = 0;
+    }
+
+
+    public static void main(String[] args) {
+        Setik setik = new Setik(10);
+        setik.add("Hello");
+        setik.add("World");
+        setik.add("Java");
+        setik.add("Python");
+        setik.add("C++");
+        setik.add("asd");
+        setik.print();
+
+        System.out.println();
+
+        System.out.println(setik.size());
+
+        setik.remove("Java");
+        setik.print();
+        System.out.println(setik.size());
+
+        setik.clear();
+
+        setik.print();
+        System.out.println(setik.size());
+    }
+
+
+
+}
